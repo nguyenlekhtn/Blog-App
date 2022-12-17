00409246af5e6e8c30ed5d631b2cb98a6a67119c
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2022-12-17 20:49:18 +0700

    validate and display errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:articles).permit(:title, :body)
+    params.require(:article).permit(:title, :body)
   end
 end
